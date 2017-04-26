d729d85f333970ba0d4486b5047c2aab9d12f889
yasukoyamashita <akubi0117@gmail.com>
2017-04-26 14:54:39 +0000

    CRUD messages

--- config/app.php
+++ config/app.php
@@ -145,6 +145,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // 追加
+        Collective\Html\HtmlServiceProvider::class,        
+        
+
     ],
 
     /*
@@ -192,6 +196,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,        
 
     ],
 
